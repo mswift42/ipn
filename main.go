dc13f5d76ccd1af58fca03cfb5cad8a22c59fd01
mswift42 <martin.haesler@gmail.com>
2016-10-08 18:16:54 +0100

    Add subtitle to Programme struct.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -9,6 +10,7 @@ import (
 type Programme struct {
 	index     int
 	name      string
+	subtitle  string
 	pid       string
 	episode   int
 	series    int
@@ -16,18 +18,26 @@ type Programme struct {
 	url       string
 }
 
-func NewProgramme(index, series, episode int, name,
+func NewProgramme(index, series, episode int, name, subtitle,
 	pid, thumbnail, url string) *Programme {
-	return &Programme{index, name, pid, episode, series, thumbnail, url}
+	return &Programme{index, name, subtitle, pid, episode, series, thumbnail, url}
 }
-
+func mostPopular() []*Programme {
+	popurl := "http://www.bbc.co.uk/iplayer/group/most-popular"
+	doc, err := goquery.NewDocument(popurl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	doc.Find(".list-item")
+	return nil
+}
 func main() {
 	doc, err := goquery.NewDocument("http://www.bbc.co.uk/iplayer/group/most-popular")
 	if err != nil {
 		fmt.Println(err)
 	}
-	doc.Find(".list-item episode numbered").Each(func(i int, s *goquery.Selection) {
-		title := s.Find("").Text()
-		fmt.Println(title)
+	doc.Find(".list-item").Each(func(i int, s *goquery.Selection) {
+		title := s.Find(".secondary > .title ").Text()
+		fmt.Println(i, title)
 	})
 }
