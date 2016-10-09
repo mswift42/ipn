c3b3d8d62a4e5de0c58a6b9857f3dbe4fe86ab99
mswift42 <martin.haesler@gmail.com>
2016-10-09 20:02:48 +0100

    Add loadTestHtml method.

--- main_test.go
+++ main_test.go
@@ -1,21 +1,37 @@
 package main
 
 import (
+	"bytes"
+	"io/ioutil"
 	"testing"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/stretchr/testify/assert"
 )
+
+func loadTestHtml(filename string) *goquery.Document {
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(file))
+	if err != nil {
+		panic(err)
+	}
+	return doc
+}
 
 func TestNewProgramme(t *testing.T) {
 	assert := assert.New(t)
 	index := 1
 	name := "programme"
+	subtitle := "subtitle"
 	pid := "123"
 	episode := 1
 	series := 1
 	thumbnail := "http://thumbnail.url"
 	url := "http://programme.url"
-	np := NewProgramme(index, series, episode, name, pid, thumbnail, url)
+	np := NewProgramme(index, series, episode, name, subtitle, pid, thumbnail, url)
 	assert.Equal(np.index, 1)
 	assert.Equal(np.pid, "123")
 	assert.Equal(np.episode, 1)
@@ -23,4 +39,7 @@ func TestNewProgramme(t *testing.T) {
 	assert.Equal(np.url, "http://programme.url")
 	assert.Equal(np.name, "programme")
 	assert.Equal(np.series, 1)
+	assert.Equal(np.subtitle, "subtitle")
+	testhtml := loadTestHtml("iplayermostpopular.html")
+	assert.NotNil(testhtml)
 }
