801a0ed8179417024ac7195814d54430db6805ab
mswift42 <martin.haesler@gmail.com>
2016-10-13 14:13:37 +0100

    add gotests generated tests

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"strings"
 	"testing"
 
@@ -91,4 +92,137 @@ func TestNewProgramme(t *testing.T) {
 	assert.Equal(np.subtitle, "subtitle")
 	testhtml := loadTestHtml("iplayermostpopular.html")
 	assert.NotNil(testhtml)
+}
+
+func Test_mostPopular(t *testing.T) {
+	tests := []struct {
+		name string
+		want []*Programme
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		if got := mostPopular(); !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%q. mostPopular() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func Test_loadTestHtml(t *testing.T) {
+	type args struct {
+		filename string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *goquery.Document
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		if got := loadTestHtml(tt.args.filename); !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%q. loadTestHtml() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func Test_findTitle(t *testing.T) {
+	type args struct {
+		s *goquery.Selection
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		if got := findTitle(tt.args.s); got != tt.want {
+			t.Errorf("%q. findTitle() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func Test_findSubtitle(t *testing.T) {
+	type args struct {
+		s *goquery.Selection
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		if got := findSubtitle(tt.args.s); got != tt.want {
+			t.Errorf("%q. findSubtitle() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func Test_findUrl(t *testing.T) {
+	type args struct {
+		s *goquery.Selection
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		if got := findUrl(tt.args.s); got != tt.want {
+			t.Errorf("%q. findUrl() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func Test_findThumbnail(t *testing.T) {
+	type args struct {
+		s *goquery.Selection
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		if got := findThumbnail(tt.args.s); got != tt.want {
+			t.Errorf("%q. findThumbnail() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func Test_findPid(t *testing.T) {
+	type args struct {
+		s *goquery.Selection
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		if got := findPid(tt.args.s); got != tt.want {
+			t.Errorf("%q. findPid() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+	// TODO: Add test cases.
+	}
+	for range tests {
+		main()
+	}
 }
