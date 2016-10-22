b98536e0dc38d98210156e6a531e58dc42e3f2f5
mswift42 <martin.haesler@gmail.com>
2016-10-22 15:00:51 +0100

    return tv programmes for a given url

--- films/films.go
+++ films/films.go
@@ -1,8 +1,16 @@
 package films
 
-import "github.com/mswift42/ipn/tv"
+import (
+	"github.com/PuerkitoBio/goquery"
+	"github.com/mswift42/ipn/tv"
+)
 
 func Programmes() ([]*tv.Programme, error) {
 	popurl := "http://www.bbc.co.uk/iplayer/categories/films/all?sort=atoz"
+	doc, err := goquery.NewDocument(popurl)
+	if err != nil {
+		return nil, err
+	}
+	var programmes []*tv.Programme
 
 }
