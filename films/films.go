7f56d8f7fc2c24db0b92cf0442c01dacd229f982
mswift42 <martin.haesler@gmail.com>
2016-10-22 15:08:49 +0100

    Generate films with tv Programmes method

--- films/films.go
+++ films/films.go
@@ -1,16 +1,12 @@
 package films
 
-import (
-	"github.com/PuerkitoBio/goquery"
-	"github.com/mswift42/ipn/tv"
-)
+import "github.com/mswift42/ipn/tv"
 
 func Programmes() ([]*tv.Programme, error) {
-	popurl := "http://www.bbc.co.uk/iplayer/categories/films/all?sort=atoz"
-	doc, err := goquery.NewDocument(popurl)
+	filmurl := "http://www.bbc.co.uk/iplayer/categories/films/all?sort=atoz"
+	programmes, err := tv.Programmes(filmurl)
 	if err != nil {
 		return nil, err
 	}
-	var programmes []*tv.Programme
-
+	return programmes, nil
 }
