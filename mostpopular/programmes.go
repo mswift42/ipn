8b1c26b9672248f5813023af345f7c1668f4adf8
mswift42 <martin.haesler@gmail.com>
2016-10-22 14:52:38 +0100

    use Programmes method in tv package

--- mostpopular/programmes.go
+++ mostpopular/programmes.go
@@ -1,48 +1,12 @@
 package mostpopular
 
-import (
-	"github.com/PuerkitoBio/goquery"
-	"github.com/mswift42/ipn/tv"
-)
+import "github.com/mswift42/ipn/tv"
 
 func Programmes() ([]*tv.Programme, error) {
 	popurl := "http://www.bbc.co.uk/iplayer/group/most-popular"
-	doc, err := goquery.NewDocument(popurl)
+	programmes, err := tv.Programmes(popurl)
 	if err != nil {
 		return nil, err
 	}
-	var programmes []*tv.Programme
-	doc.Find(".list-item").Each(func(i int, s *goquery.Selection) {
-		title := findTitle(s)
-		subtitle := findSubtitle(s)
-		synopsis := findSynopsis(s)
-		pid := findPid(s)
-		thumbnail := findThumbnail(s)
-		url := findUrl(s)
-		programmes = append(programmes, tv.NewProgramme(title, subtitle, synopsis, pid,
-			thumbnail, url))
-
-	})
-	return programmes, nil
-}
-
-func findTitle(s *goquery.Selection) string {
-	return s.Find(".secondary > .title").Text()
-}
-
-func findSubtitle(s *goquery.Selection) string {
-	return s.Find(".secondary > .subtitle").Text()
-}
-
-func findUrl(s *goquery.Selection) string {
-	return "www.bbc.co.uk" + s.Find("a").AttrOr("href", "")
-}
-func findThumbnail(s *goquery.Selection) string {
-	return s.Find(".r-image").AttrOr("data-ip-src", "")
-}
-func findPid(s *goquery.Selection) string {
-	return s.Find(".list-item-inner > a").AttrOr("data-episode-id", "")
-}
-func findSynopsis(s *goquery.Selection) string {
-	return s.Find(".synopsis").Text()
+	return programmes, err
 }
