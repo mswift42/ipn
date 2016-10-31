9e7a292c40dfafe1e2fce58b53585309e1a52080
mswift42 <martin.haesler@gmail.com>
2016-10-31 19:12:35 +0000

    Take goquery.Document as argument.

--- drama-crime/drama_crime.go
+++ drama-crime/drama_crime.go
@@ -5,12 +5,7 @@ import (
 	"github.com/mswift42/ipn/tv"
 )
 
-func Programmes(doc *goquery.Document) ([]*tv.Programme, error) {
-	crimeurl := "http://www.bbc.co.uk/iplayer/categories/drama-crime/all?sort=atoz"
-	doc, err := goquery.NewDocument(crimeurl)
-	if err != nil {
-		return nil, err
-	}
+func Programmes(doc *goquery.Document) []*tv.Programme {
 	programmes := tv.Programmes(doc)
-	return programmes, err
+	return programmes
 }
