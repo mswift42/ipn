19096a3b39694fb23dab472739999de0a8d9ffc5
mswift42 <martin.haesler@gmail.com>
2016-10-31 19:12:13 +0000

    Add some checks for films document

--- films/films_test.go
+++ films/films_test.go
@@ -3,11 +3,19 @@ package films
 import (
 	"testing"
 
+	"github.com/mswift42/ipn/testutils"
 	"github.com/stretchr/testify/assert"
 )
 
+const films = "films.html"
+
 func TestProgrammes(t *testing.T) {
-	programmes, err := Programmes()
-	assert.Nil(t, err)
-	assert.NotEmpty(t, programmes)
+	assert := assert.New(t)
+	filmdoc := testutils.LoadTestHtml(films)
+	programmes := Programmes(filmdoc)
+	assert.NotEmpty(programmes)
+	assert.Equal(programmes[0].Title, "Adam Curtis")
+	assert.Equal(programmes[0].Subtitle, "HyperNormalisation")
+	assert.Equal(programmes[2].Title, "Begin Again")
+	assert.Equal(programmes[2].Synopsis, "A former music company producer and a talented singer-songwriter start to collaborate.")
 }
