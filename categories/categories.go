bf3fa3eb2b735d6339755d07ec7928e180717a2f
mswift42 <martin.haesler@gmail.com>
2016-11-04 18:29:38 +0000

    add mostpopular to categories.

--- categories/categories.go
+++ categories/categories.go
@@ -1,8 +1,21 @@
 package categories
 
+import (
+	"github.com/PuerkitoBio/goquery"
+	"github.com/mswift42/ipn/tv"
+)
+
 const (
 	mp      = "http://www.bbc.co.uk/iplayer/group/most-popular"
 	flms    = "http://www.bbc.co.uk/iplayer/categories/films/all?sort=atoz"
 	crdrama = "http://www.bbc.co.uk/iplayer/categories/drama-crime/all?sort=atoz"
 	cmdy    = "http://www.bbc.co.uk/iplayer/categories/comedy/all?sort=atoz"
 )
+
+func MostPopular(doc *goquery.Document) ([]*tv.Programme, error) {
+	doc, err := goquery.NewDocument(mp)
+	if err != nil {
+		return nil, err
+	}
+	return tv.Programmes(doc), nil
+}
