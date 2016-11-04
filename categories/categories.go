7b44f54c6e5b1e6cea969d24dabbee42639901d9
mswift42 <martin.haesler@gmail.com>
2016-11-04 18:23:46 +0000

    Add urls for categories.

--- categories/categories.go
+++ categories/categories.go
@@ -1,1 +1,8 @@
 package categories
+
+const (
+	mp      = "http://www.bbc.co.uk/iplayer/group/most-popular"
+	flms    = "http://www.bbc.co.uk/iplayer/categories/films/all?sort=atoz"
+	crdrama = "http://www.bbc.co.uk/iplayer/categories/drama-crime/all?sort=atoz"
+	cmdy    = "http://www.bbc.co.uk/iplayer/categories/comedy/all?sort=atoz"
+)
