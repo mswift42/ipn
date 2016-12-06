f9922d7a01e5966a99d3bcf21a784bcfcb66c7d8
mswift42 <martin.haesler@gmail.com>
2016-12-06 15:21:19 +0000

    Add method testHtmlURL

--- testutils/testutils.go
+++ testutils/testutils.go
@@ -7,6 +7,20 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type testHtmLURL string
+
+func (th testHtmLURL) urlDoc() (*goquery.Document, error) {
+	file, err := ioutil.ReadFile(string(th))
+	if err != nil {
+		return nil, err
+	}
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(file))
+	if err != nil {
+		return nil, err
+	}
+	return doc, nil
+}
+
 func LoadTestHtml(filename string) *goquery.Document {
 	file, err := ioutil.ReadFile(filename)
 	if err != nil {
