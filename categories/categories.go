c87851c6d38224e4c794cac07d2478aeccb8b1c4
mswift42 <martin.haesler@gmail.com>
2016-12-07 17:15:27 +0000

    Fix spelling for categories constants.

--- categories/categories.go
+++ categories/categories.go
@@ -1,15 +1,10 @@
 package categories
 
-import (
-	"github.com/PuerkitoBio/goquery"
-	"github.com/mswift42/ipn/tv"
-)
-
 const (
-	mostpop = "http://www.bbc.co.uk/iplayer/group/most-popular"
-	flms    = "http://www.bbc.co.uk/iplayer/categories/films/all?sort=atoz"
-	crdrama = "http://www.bbc.co.uk/iplayer/categories/drama-crime/all?sort=atoz"
-	cmdy    = "http://www.bbc.co.uk/iplayer/categories/comedy/all?sort=atoz"
+	mostpopular = "http://www.bbc.co.uk/iplayer/group/most-popular"
+	films       = "http://www.bbc.co.uk/iplayer/categories/films/all?sort=atoz"
+	crimedrama  = "http://www.bbc.co.uk/iplayer/categories/drama-crime/all?sort=atoz"
+	comedy      = "http://www.bbc.co.uk/iplayer/categories/comedy/all?sort=atoz"
 )
 
 type Category struct {
@@ -18,20 +13,4 @@ type Category struct {
 
 func newCategory(name, url string) *Category {
 	return &Category{name, url}
-}
-
-func mostPopular(doc *goquery.Document) []*tv.Programme {
-	return tv.Programmes(doc)
-}
-
-func films(doc *goquery.Document) []*tv.Programme {
-	return tv.Programmes(doc)
-}
-
-func cdrdama(doc *goquery.Document) []*tv.Programme {
-	return tv.Programmes(doc)
-}
-
-func comedy(doc *goquery.Document) []*tv.Programme {
-	return tv.Programmes(doc)
 }
