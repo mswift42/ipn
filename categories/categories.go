3d5c59af7c5be79ce085d6291fb80b3071f71cd6
mswift42 <martin.haesler@gmail.com>
2016-12-14 17:26:17 +0000

    Add method Ac to test concurrent download of programmes.

--- categories/categories.go
+++ categories/categories.go
@@ -1,6 +1,10 @@
 package categories
 
-import "github.com/mswift42/ipn/tv"
+import (
+	"fmt"
+
+	"github.com/mswift42/ipn/tv"
+)
 
 const (
 	mostpopular = "http://www.bbc.co.uk/iplayer/group/most-popular"
@@ -10,24 +14,55 @@ const (
 )
 
 func category(url string, c chan []*tv.Programme) {
-	beeburl := tv.Beeburl(url)
-	c <- tv.Programmes(beeburl)
+	beeburl := tv.BeebURL(url)
+	prog, err := tv.Programmes(beeburl)
+	if err != nil {
+		panic(err)
+	}
+	c <- prog
 }
 
 func AllCategories() ([]*tv.Programme, error) {
 	categories := []string{
 		mostpopular, films, crimedrama, comedy,
 	}
-	var beeburl tv.BeebURL
 	programmes := make([]*tv.Programme, len(categories))
+	c := make(chan []*tv.Programme)
 	for _, i := range categories {
-		beeburl = tv.BeebURL(i)
-		programme, err := tv.Programmes(beeburl)
-		if err != nil {
-			return nil, err
-		}
-		programmes = append(programmes, programme...)
+		go func(i string) {
+			category(i, c)
 
+			programmes = append(programmes, <-c...)
+
+		}(i)
+	}
+	return programmes, nil
+
+}
+func Ac() ([]*tv.Programme, error) {
+	categories := []string{
+		mostpopular, films, crimedrama, comedy,
+	}
+	programmes := make([]*tv.Programme, len(categories))
+	ch := make(chan []*tv.Programme)
+	for _, i := range categories {
+		go func(i string) {
+			fmt.Println("Fetching Cat: ", i)
+			beeburl := tv.BeebURL(i)
+			prog, err := tv.Programmes(beeburl)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("Programmes: ", prog[0].Title)
+			ch <- prog
+		}(i)
+	}
+	for {
+		select {
+		case r := <-ch:
+			fmt.Println("Category was fetched: ", r[0].Title)
+			programmes = append(programmes, r...)
+		}
 	}
 	return programmes, nil
 
