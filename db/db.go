c7a45950bca5e90eca544fcb39c2d71c953811be
mswift42 <martin.haesler@gmail.com>
2017-01-15 13:22:07 +0000

    Add Method ListAVailableCategories.

--- db/db.go
+++ db/db.go
@@ -84,6 +84,16 @@ func (pdb *programmeDB) findCategory(cat string) (*tv.Category, error) {
 	return nil, errors.New("Can not find Category with Name: " + cat)
 }
 
+// ListAvailableCategories returns a line seperated string of
+// all Categories saved in the Programme DB.
+func (pdb *programmeDB) ListAvailableCategories() string {
+	var buffer bytes.Buffer
+	for _, i := range pdb.Categories {
+		buffer.WriteString(i.Name + "\n")
+	}
+	return buffer.String()
+}
+
 // FindTitle returns a line seperated String of all Programmes
 // containing a given string in its Title. Case distinction is ignored.
 func (pdb *programmeDB) FindTitle(cat string) string {
