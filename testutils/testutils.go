1f773b9dd2d6ebe57b3b1dd0db1e0fce5c3550f1
mswift42 <martin.haesler@gmail.com>
2017-09-21 12:10:26 +0100

    implement loadDocument method.

--- testutils/testutils.go
+++ testutils/testutils.go
@@ -5,11 +5,23 @@ import (
 	"io/ioutil"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/mswift42/ipn/tv"
 )
 type TestHtmlURL struct {
 	url string
 }
 
+func (th TestHtmlURL) loadDocument() (*tv.IplayerDocument, error) {
+	file, err := ioutil.ReadFile(th.url)
+	if err != nil {
+		return nil, err
+	}
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(file))
+	if err != nil {
+		return nil, err
+	}
+	return doc, nil
+}
 
 func (th TestHtmlURL) UrlDoc() (*goquery.Document, error) {
 	file, err := ioutil.ReadFile(th.url)
