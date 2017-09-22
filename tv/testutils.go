7dad3f1b042363b225a39304c0ee5525fcf1fe92
mswift42 <martin.haesler@gmail.com>
2017-09-22 11:57:50 +0100

    move testutils to folder / package tv.

---
+++ tv/testutils.go
@@ -0,0 +1,24 @@
+package tv
+
+import (
+	"bytes"
+	"io/ioutil"
+
+	"github.com/mswift42/goquery"
+)
+
+type TestHtmlURL struct {
+	url string
+}
+
+func (th TestHtmlURL) loadDocument() (*IplayerDocument, error) {
+	file, err := ioutil.ReadFile(th.url)
+	if err != nil {
+		return nil, err
+	}
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(file))
+	if err != nil {
+		return nil, err
+	}
+	return NewIplayerDocument(doc), nil
+}
